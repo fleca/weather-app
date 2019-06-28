75e82f3b6c3ba322bc27825792be9ab54fad7532
fleca <barroso31>
2019-06-28 00:42:15 -0300

    Added callback abstraction and callback chaining to the app pt.2

--- app.js
+++ app.js
@@ -8,17 +8,17 @@ const address = process.argv[2]
 if (!address) {
   log(chalk.red('Please provide an anddress'))
 } else {
-  geocode(address, (error, data) => {
+  geocode(address, (error, { latitude, longitude, location }) => {
     if (error) {
       return log(chalk.red(error))
     }
   
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+    forecast(latitude, longitude, (error, forecastData) => {
       if (error) {
         return log(chalk.red(error))
       }
   
-      log(chalk.bgBlue(data.location))
+      log(chalk.bgBlue(location))
       log(chalk.blue(forecastData))
     })
   })
--- utils/forecast.js
+++ utils/forecast.js
@@ -3,15 +3,15 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
   const url = `https://api.darksky.net/forecast/955761bedb39ea039a8693464a687d44/${latitude},${longitude}?exclude=minutely,hourly,alerts,flags&units=si`
   
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback('Unable to connect to weather service!')
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback('Unable to find location')
     } else {
-      const currentlyData = response.body.currently
+      const currentlyData = body.currently
   
-      callback(undefined, `${response.body.daily.data[0].summary}\nIt is currently ${currentlyData.temperature}°C out. There is a ${currentlyData.precipProbability}% chance of rain.`)
+      callback(undefined, `${body.daily.data[0].summary}\nIt is currently ${currentlyData.temperature}°C out. There is a ${currentlyData.precipProbability}% chance of rain.`)
     }
   })
 }
